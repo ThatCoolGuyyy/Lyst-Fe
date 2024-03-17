aaab584279a108feb66c3776930e271155f55c25
ThatCoolGuyyy <goldendarmie@gmail.com>
2024-03-17 23:37:24 +0100

    Added a dashbaord view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,76 @@
+<template>
+    <NavBar/>
+    
+      <TransitionRoot as="template" :show="open">
+      <Dialog as="div" class="relative z-10" @click="open = !open">
+        <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
+  
+        <div class="fixed inset-0 overflow-hidden">
+          <div class="absolute inset-0 overflow-hidden">
+            <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
+                <DialogPanel class="pointer-events-auto relative w-screen max-w-md">
+                  <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
+                    <div class="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
+                      <button type="button" class="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white" @click="open = false">
+                        <span class="absolute -inset-2.5" />
+                        <span class="sr-only">Close panel</span>
+                        <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </TransitionChild>
+                  <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                    <div class="px-4 sm:px-6">
+                        <button @click="open = true">
+                            <DialogTitle class="text-base font-semibold leading-6 text-gray-900">Panel title</DialogTitle>
+                        </button>
+                    </div>
+                    <div class="relative mt-6 flex-1 px-4 sm:px-6">
+                      <!-- Your content -->
+                    </div>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+    <AsideMenu
+    :is-aside-mobile-expanded="isAsideMobileExpanded"
+    :is-aside-lg-active="isAsideLgActive"
+    :menu="menuAside"
+    @menu-click="menuClick"
+    @aside-lg-close-click="isAsideLgActive = false"
+    />
+  </template>
+  
+  <script>
+import { ref } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+import NavBar from '@/components/NavBar.vue';
+import AsideMenu from '@/components/AsideMenu.vue';
+
+
+export default {
+  data() {
+    return {
+      open: true 
+    };
+  },
+  components: {
+    Dialog, 
+    DialogPanel, 
+    DialogTitle, 
+    TransitionChild, 
+    TransitionRoot, 
+    XMarkIcon,
+    NavBar,
+    AsideMenu
+  }
+};
+</script>
+  
