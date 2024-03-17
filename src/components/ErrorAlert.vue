48bfbc6e5b05afd7db80574cde39ac3d5b46d7ee
ThatCoolGuyyy <goldendarmie@gmail.com>
2024-03-17 23:35:49 +0100

    Added Error and Success popups

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="show" class="bg-red-200 text-red-800 p-3 rounded">
+      {{ msg }}
+      <button @click="dismiss">Dismiss</button> 
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      show: {
+        type: Boolean, 
+        default: false 
+      },
+      msg: {
+        type: String,
+        required: true
+      }
+
+    },
+    methods: {
+      dismiss() {
+        this.$emit('dismiss');
+      }
+    }
+  }
+  </script>
+  
---
+++ src/components/SuccessAlert.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="show" class="bg-green-200 text-green-800 p-3 rounded">
+      {{ msg }}
+      <button @click="dismiss">Dismiss</button> 
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      show: {
+        type: Boolean, 
+        default: false 
+      },
+      msg: {
+        type: String,
+        required: true
+      }
+
+    },
+    methods: {
+      dismiss() {
+        this.$emit('dismiss');
+      }
+    }
+  }
+  </script>
+  
