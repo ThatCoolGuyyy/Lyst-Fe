9eb01cb209ab192aa87f5b496ec3fb526250e542
ThatCoolGuyyy <goldendarmie@gmail.com>
2024-03-17 23:36:15 +0100

    Created the landing page component

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <NavBar/>
+    <div>
+        <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Property Management Tool For Businesses</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From tenant onboadrding and manangement to property listings, businesses around the world use Lyst to simplify their Property Management.</p>
+            <RouterLink to="/login" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get started
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </RouterLink>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
+        </div>                
+    </div>
+</section>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+    export default {
+        name: 'LandingPage',
+        components: {
+            NavBar
+        }
+    }
+</script>
